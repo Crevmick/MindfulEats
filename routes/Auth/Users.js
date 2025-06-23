426c33940ae7b97704e772510cc8a8482b9c6d62
Muhyideen Abdulbasit Kolawole <muhyideenabdulbasit11@gmail.com>
2025-06-24 02:56:03 +0900

    add req.user

--- routes/Auth/Users.js
+++ routes/Auth/Users.js
@@ -11,4 +11,25 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/me', async (req, res) => {
+  try {
+    const userId = req.user.id;
+    if (!userId) {
+    res.status(400).json({ message: "You need to login" });
+    }
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+
+    res.status(200).json(user);
+
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 export default router;
--- routes/Mood/moodLogRoute.js
+++ routes/Mood/moodLogRoute.js
@@ -1,12 +1,10 @@
 import express from 'express';
 import { createMoodLog, getAllMoodLogs, getMoodLogsByUser } from '../../Controller/moodLogController.js';
-import authenticateUser from '../../middleware/authenticateUser.js';
-
 
 const router = express.Router();
 
-router.post('/', authenticateUser, createMoodLog); //This help to Create a mood log
-router.get('/', authenticateUser, getAllMoodLogs); //This help to Get all mood logs
-router.get('/user', authenticateUser, getMoodLogsByUser); // Get mood logs by user
+router.post('/', createMoodLog); //This help to Create a mood log
+router.get('/', getAllMoodLogs); //This help to Get all mood logs
+router.get('/user/:userId', getMoodLogsByUser); // Get mood logs by user
 
 export default router;
