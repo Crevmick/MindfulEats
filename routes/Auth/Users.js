4529ced7345b86685d6e132645cb44242e1c49c2
Muhyideen Abdulbasit Kolawole <muhyideenabdulbasit11@gmail.com>
2025-06-24 03:02:16 +0900

    add req.user

--- routes/Auth/Users.js
+++ routes/Auth/Users.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import User from '../../model/User.js';
+import authenticateUser from '../../middleware/authenticateUser.js';
+
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get('/', authenticateUser, async (req, res) => {
   try {
     const users = await User.find();
     res.json(users);
@@ -11,7 +13,7 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/me', async (req, res) => {
+router.get('/me', authenticateUser, async (req, res) => {
   try {
     const userId = req.user.id;
     if (!userId) {
@@ -24,12 +26,39 @@ router.get('/me', async (req, res) => {
       return res.status(404).json({ message: "User not found" });
     }
 
-
     res.status(200).json(user);
 
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 });
+
+router.patch('/update-profile', authenticateUser, async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    // Get fields user wants to update
+    const { fullName, email } = req.body;
+
+    // Find the user
+    const user = await User.findById(userId);
+
+    if (!user) return res.status(404).json({ message: 'User not found' });
+
+    // Update allowed fields
+    user.fullName = fullName || user.fullName;
+    user.email = email || user.email;
+
+    await user.save();
+
+    res.status(200).json({
+      message: 'Profile updated successfully',
+      user
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 
 export default router;
